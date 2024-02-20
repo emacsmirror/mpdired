ca34f598247693bced0c6798b6a1b5352fe6d580
Manuel Giraud <manuel@ledu-giraud.fr>
2024-02-20 11:13:23 +0100

    insert header

--- mpdired.el
+++ mpdired.el
@@ -1,3 +1,38 @@
+;;; mpdired.el --- A dired-like client for Music Player Daemon -*- lexical-binding: t -*-
+
+;; Copyright (C) 2024  Manuel Giraud
+
+;; Author: Manuel Giraud <manuel@ledu-giraud.fr>
+;; Keywords: multimedia
+
+;; SPDX-License-Identifier: GPL-3.0-or-later
+
+;; This file is NOT part of GNU Emacs.
+
+;; GNU Emacs is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; This a client for the Music Player Daemon (mpd) with interactions
+;; inspired from Dired.  It features two views packed into the same
+;; interactive buffer: the browser view and the queue view.
+
+;;; Bugs:
+;;
+
+;;; Code:
+
 (defcustom mpdired-host (or (getenv "MPD_HOST") "localhost")
   "Host for MPD."
   :type 'string)
